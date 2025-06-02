0838f677838d46d8c7fd1aa09b9f0434927ebf31
Neo <surendarraja420@gmail.com>
2025-06-02 22:49:07 +0530

    Initial commit

---
+++ src/main/java/com/neo/strider/RedisLiteApplication.java
@@ -0,0 +1,13 @@
+package com.neo.strider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisLiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neo/strider/RedisLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo.strider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
